1654426af0139a720d9612881c0db373c5df5888
zhxing <iloveryoko@qq.com>
2017-08-16 16:18:57 +0800

    Timer init

---
+++ timer/main.c
@@ -0,0 +1,24 @@
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include "thread_pool.h"
+#include <getopt.h>
+//#include "thread_pool.c"
+
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
